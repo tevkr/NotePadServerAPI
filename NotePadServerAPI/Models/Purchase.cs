9bc64a34410b79584f4cd60616def28a2709b5c8
nom <vlad.nomerov@yandex.ru>
2021-03-09 12:44:25 +0300

    Добавил файлы контроллеров, DAO и моделей.

---
+++ NotePadServerAPI/Models/Purchase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NotePadServerAPI.Models
+{
+    public class Purchase
+    {
+        public uint userId { get; set; }
+        public uint id { get; set; }
+        public DateTime purchaseTime { get; set; }
+        public string name { get; set; }
+        public double cost { get; set; }
+        public Purchase() { }
+        public Purchase(uint userId, DateTime purchaseTime, string name, double cost)
+        {
+            this.userId = userId;
+            this.purchaseTime = purchaseTime;
+            this.name = name;
+            this.cost = cost;
+        }
+        public Purchase(uint userId, uint id, DateTime purchaseTime, string name, double cost)
+        {
+            this.userId = userId;
+            this.id = id;
+            this.purchaseTime = purchaseTime;
+            this.name = name;
+            this.cost = cost;
+        }
+    }
+}
---
+++ NotePadServerAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace NotePadServerAPI.Models
+{
+    public class User
+    {
+        public uint id { get; set; }
+        public string name { get; set; }
+        public User() { }
+        public User(string name)
+        {
+            this.name = name;
+        }
+        public User(uint id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
